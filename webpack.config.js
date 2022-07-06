97a0414b55fb046b21b3cea4debc4f463dd46118
Mariana Svirzhevska <mariana_svirzhevska@epam.com>
2022-07-06 21:28:03 +0300

    WH4 Task 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const miniCss = require('mini-css-extract-plugin');
+const CriticalCssPlugin = require('critical-css-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -12,14 +13,14 @@ module.exports = {
         rules: [
             {
                 test: /\.html$/i,
-                loader: "html-loader",
+                loader: 'html-loader',
             },
             {
                 test: /\.(scss)$/,
                 use: [
                     miniCss.loader,
-                    "css-loader",
-                    "sass-loader",
+                    'css-loader',
+                    'sass-loader',
                 ]
             },
             {
@@ -33,10 +34,26 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
+            template: './src/index.html'
         }),
         new miniCss({
             filename: 'style.css',
+        }),
+        new CriticalCssPlugin({
+            dimensions: [
+                {
+                  height: 320,
+                  width: 568,
+                },
+                {
+                  height: 768,
+                  width: 1024,
+                },
+                {
+                  height: 1920,
+                  width: 1080,
+                },
+              ],
         }),
     ],
 };
